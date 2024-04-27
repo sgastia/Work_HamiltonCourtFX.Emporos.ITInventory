8e00433f57a17725dd0edc230be848bb760d9736
sebastian surface <sebastian.gastiazoro@hotmail.com>
2024-04-27 23:28:34 +0200

    I added navigation with the menu.

--- hamiltoncourtfx.emporos.itinventory.client/src/app/components/menu/menu.component.ts
+++ hamiltoncourtfx.emporos.itinventory.client/src/app/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -11,5 +12,14 @@ import { MatButtonModule } from '@angular/material/button';
 })
 
 export class MenuComponent {
+  constructor(private router: Router) { }
 
+  navigateMenu(tag: string) {
+    if (tag === 'Employees') {
+      this.router.navigate(['/list-employees']);
+    }
+    if (tag === 'Devices') {
+      this.router.navigate(['/list-devices']);
+    }
+  }
 }
--- hamiltoncourtfx.emporos.itinventory.client/src/app/components/shared.module.ts
+++ hamiltoncourtfx.emporos.itinventory.client/src/app/components/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { MenuComponent } from "./menu/menu.component";
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from "@angular/router";
 
 
 @NgModule({
   declarations: [],
-  imports: [MenuComponent, HeaderComponent],
+  imports: [MenuComponent, HeaderComponent, RouterModule],
   exports: [MenuComponent, HeaderComponent] 
 })
 export class SharedModule { }
