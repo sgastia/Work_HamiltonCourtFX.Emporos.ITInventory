b19fe93b37383ab85e894d7479a380dd7b9df095
sebastian surface <sebastian.gastiazoro@hotmail.com>
2024-04-29 17:18:43 +0200

    It shows all devices in employee detail.

--- hamiltoncourtfx.emporos.itinventory.client/src/app/forms/employees/detail-employee/detail-employee.component.ts
+++ hamiltoncourtfx.emporos.itinventory.client/src/app/forms/employees/detail-employee/detail-employee.component.ts
@@ -1,25 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router, RouterModule } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { EmployeeService } from '../../../services/employee.service';
 import { EmployeeInterface } from '../../../interfaces/employee.interface';
+import { DeviceInterface } from '../../../interfaces/device.interface';
+import { DeviceService } from '../../../services/device.service';
 
 @Component({
   selector: 'app-detail-employee',
   templateUrl: './detail-employee.component.html',
   styleUrls: ['./detail-employee.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule]
+  imports: [FormsModule, CommonModule, RouterModule,]
 })
 export class DetailEmployeeComponent implements OnInit {
   employeeItem$!: Observable<EmployeeInterface>;
+  devicesList$!: Observable<DeviceInterface[]>;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private employeeService: EmployeeService) {
+    private employeeService: EmployeeService,
+    private devicesService: DeviceService) {
 
   }
 
@@ -29,6 +33,12 @@ export class DetailEmployeeComponent implements OnInit {
         const id = params.get('id')!;
         return this.employeeService.getEmployeeById(id);
       }));
+
+    this.devicesList$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        return this.devicesService.getAllDevices();
+      })
+    );
   }
 
   gotoList() {
