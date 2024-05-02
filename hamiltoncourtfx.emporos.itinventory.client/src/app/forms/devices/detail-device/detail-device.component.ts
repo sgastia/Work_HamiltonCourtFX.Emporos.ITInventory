2d2305065e97c95ba026a19e14bd23dc1af03cc1
sebastian surface <sebastian.gastiazoro@hotmail.com>
2024-05-02 19:48:33 +0200

    I failed to stylish with mat-form-field.

--- hamiltoncourtfx.emporos.itinventory.client/src/app/forms/devices/detail-device/detail-device.component.ts
+++ hamiltoncourtfx.emporos.itinventory.client/src/app/forms/devices/detail-device/detail-device.component.ts
@@ -5,17 +5,20 @@ import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { DeviceService } from '../../../services/device.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-detail-device',
   templateUrl: './detail-device.component.html',
   styleUrls: ['./detail-device.component.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule]
+  imports: [FormsModule, CommonModule, MatFormFieldModule, MatInputModule, MatIconModule]
 })
 export class DetailDeviceComponent implements OnInit {
   deviceItem$!: Observable<DeviceInterface>;
-
+  deviceId: string = "";
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -24,12 +27,17 @@ export class DetailDeviceComponent implements OnInit {
     }
 
   ngOnInit(): void {
+    
     this.deviceItem$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
-        const id = params.get('id')!;
-        return this.deviceService.getDeviceById(id);
+        this.deviceId = params.get('id')!;    
+        return this.deviceService.getDeviceById(this.deviceId);
       }));
     }
+
+  save() {
+    console.log('Id: ' + this.deviceId)
+  }
 
   gotoList() {
     this.router.navigate(['/list-devices']);
