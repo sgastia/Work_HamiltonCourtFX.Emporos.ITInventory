bb8cc1cca04934f61c5cd0328dae6243303d8093
sebastian surface <sebastian.gastiazoro@hotmail.com>
2024-05-02 18:10:11 +0200

    I added content to the header and I moved the menu to the header.

--- hamiltoncourtfx.emporos.itinventory.client/src/app/components/header/header.component.ts
+++ hamiltoncourtfx.emporos.itinventory.client/src/app/components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MenuComponent]
+
 })
 export class HeaderComponent {
 
